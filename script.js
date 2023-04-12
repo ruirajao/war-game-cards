3698ca55cc15bb6e8006272735d0ccbf61bb5040
ruirajao <122367154+ruirajao@users.noreply.github.com>
2023-04-12 10:52:55 +0100

    - working on first time play() method is called

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 const suits = ['spade', 'club', 'heart', 'diamond'];
 const suitsSymbol = ['♠', '♣', '♥', '♦'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'];
+const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const valuesSymbol = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 let deck = [];
 
 
-//------------Cards deck with all properties------------
+//------------DECK: Cards deck with all properties------------
 suits.forEach((suit, suitIndex) => {
     values.forEach((value, valuesIndex) => {
         deck.push({
@@ -18,167 +18,238 @@ suits.forEach((suit, suitIndex) => {
         });
     })
 });
+console.log("Before shuffle:")
 console.log(deck);
 
-//------------ Array with 52 html cards------------
-let visualDeck = [];
-deck.forEach(card => {
-    visualDeck.push(
-        `<div class="card-${card.suit}">
+
+//------------VISUAL DECK: Array with 52 html cards------------
+// let visualDeck = [];
+// deck.forEach(card => {
+//     visualDeck.push(
+//         `<div class="card-${card.suit}">
+//             <div class="top-left-values">
+//               <p class="value">${card.valueSymbol}</p>
+//               <p class="suit">${card.suitSymbol}</p>
+//             </div>
+//             <div class="middle-suit">
+//               <p class="suit">${card.suitSymbol}</p>
+//             </div>
+//             <div class="bottom-right-values">
+//               <p class="value">${card.valueSymbol}</p>
+//               <p class="suit">${card.suitSymbol}</p>
+//             </div>
+//             <div class="card-back">
+//             <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
+//             </div>
+//         </div>`
+//     )
+// })
+// console.log(visualDeck);
+
+
+//------------Play(): GAME LOGIC ----------
+
+let counter = 0;
+function play() {
+    console.log("Hello world!");
+    console.log("counter:" + counter);
+    let shuffleDeck = [];
+    const p1Deck = [];
+    const p2Deck = [];
+    let tempWonCards = [];
+
+
+    if (counter === 0) {
+        shuffledDeck = shuffle(deck);
+        dealPlayerDecks(shuffledDeck, p1Deck, p2Deck);
+        drawFirstCards(p1Deck, p2Deck);
+        drawFakeStacks(p1Deck, p2Deck);
+        console.log("p1deck.length: " + p1Deck.length + "| p2deck.length: " + p2Deck
+            .length);
+        console.log("P1:" + p1Deck[0].value + "|" + "P2:" + p2Deck[0].value);
+
+        counter++;
+
+        //Player 1 win
+        if (p1Deck[0].value > p2Deck[0].value) {
+            tempWonCards.push(p2Deck.shift);
+
+            while (tempWonCards.length > 0) {
+                p1Deck.push(tempWonCards.shift());
+            }
+            
+            console.log("Player 1 won");
+
+            //Player 2 win
+        } else if (p2Deck[0].value > p1Deck[0].value) {
+            tempWonCards.push(p2Deck.shift);
+
+            while (tempWonCards.length > 0) {
+                p2Deck.push(tempWonCards.shift());
+            }
+
+            console.log("Player 2 won");
+
+            //WAR
+        } else {
+            console.log("WAR!");
+
+            if (p1Deck.length < 3) {
+                //TODO
+                console.log("P1: Not enough cards to play WAR!");
+                return;
+            }
+            if (p2Deck.length < 3) {
+                //TODO
+                console.log("P2: Not enough cards to play WAR!");
+                return;
+            }
+
+            for (let i = 0; i <= 2; i++) {
+                tempWonCards.push(p1Deck.shift());
+                tempWonCards.push(p2Deck.shift());
+            }
+            console.log("tempWonCards: " + JSON.stringify(tempWonCards));
+        }
+        console.log("counter:"+ counter);
+        return;
+    }
+    
+    if (counter=>1) {
+        console.log("Time to play the game!")
+        counter++;
+        console.log("counter:"+ counter);
+    }
+
+    //DealEachCard();
+    //CheckValues();
+    //CheckWinner();
+    //CheckWar();
+    //Deal3BackCards() & DealEachCard();
+    //UpdateCardsLeft();       
+    
+}
+
+
+
+//------------Shuffled deck (only html) ------------
+
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+
+
+//------------Deal deck for each player (only html)------------
+
+function dealPlayerDecks(anyDeck, deck1, deck2) {
+    const halfDeckLength = Math.floor(anyDeck.length / 2);
+    let firstHalf = anyDeck.slice(0, halfDeckLength);
+    let secondHalf = anyDeck.slice(halfDeckLength);
+    deck1.push(...firstHalf);
+    deck2.push(...secondHalf);
+}
+
+//  Draws first card on board
+function drawFirstCards(deck1, deck2) {
+    p1Card = deck1[0];
+    console.log(p1Card);
+    p1FirstCard = `
+        <div class="card-${p1Card.suit}">
             <div class="top-left-values">
-              <p class="value">${card.valueSymbol}</p>
-              <p class="suit">${card.suitSymbol}</p>
+              <p class="value">${p1Card.valueSymbol}</p>
+              <p class="suit">${p1Card.suitSymbol}</p>
             </div>
             <div class="middle-suit">
-              <p class="suit">${card.suitSymbol}</p>
+              <p class="suit">${p1Card.suitSymbol}</p>
             </div>
             <div class="bottom-right-values">
-              <p class="value">${card.valueSymbol}</p>
-              <p class="suit">${card.suitSymbol}</p>
+              <p class="value">${p1Card.valueSymbol}</p>
+              <p class="suit">${p1Card.suitSymbol}</p>
             </div>
             <div class="card-back">
             <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
             </div>
-        </div>`
-    )
-})
-console.log(visualDeck);
+        </div>`;
+    console.log(p1FirstCard);
+    const p1Play = document.getElementById('player-1-play');
+    p1Play.innerHTML = p1FirstCard;
 
-//------------Shuffled deck (only html) ------------
-let k = 0;
-let randomNumber = Math.floor(Math.random() * 52);
-
-function shuffle(anyDeck) {
-    for (let i = anyDeck.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [anyDeck[i], anyDeck[j]] = [anyDeck[j], anyDeck[i]];
-    }
-    return anyDeck;
+    p2Card = deck2[0];
+    p2FirstCard = `
+        <div class="card-${p2Card.suit}">
+            <div class="top-left-values">
+              <p class="value">${p2Card.valueSymbol}</p>
+              <p class="suit">${p2Card.suitSymbol}</p>
+            </div>
+            <div class="middle-suit">
+              <p class="suit">${p2Card.suitSymbol}</p>
+            </div>
+            <div class="bottom-right-values">
+              <p class="value">${p2Card.valueSymbol}</p>
+              <p class="suit">${p2Card.suitSymbol}</p>
+            </div>
+            <div class="card-back">
+            <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
+            </div>
+        </div>`;
+    const p2Play = document.getElementById('player-2-play');
+    p2Play.innerHTML = p2FirstCard;
 }
 
-const shuffledDeck = shuffle(visualDeck);
-
-
-//------------Shuffled deck for each player (only html)------------
-const p1Deck = [];
-const p2Deck = [];
-
-
-function deal(anyDeck, deck1, deck2) {
-    const halfDeckLength = Math.floor(anyDeck.length / 2);
-    let firstHalf = anyDeck.slice(0, halfDeckLength);
-    let secondHalf = anyDeck.slice(halfDeckLength);
-    p1Deck.push(...firstHalf);
-    p2Deck.push(...secondHalf);
-}
-
-deal(shuffledDeck, p1Deck, p2Deck);
-
-// const p1DeckContainer = document.getElementById('player1Deck-container');
-// p1DeckContainer.innerHTML = p1Deck.join('');
-
-// const p2DeckContainer = document.getElementById('player2Deck-container');
-// p2DeckContainer.innerHTML = p2Deck.join('');
-
-p1FirstCard = p1Deck[0];
-const p1Play = document.getElementById('player-1-play');
-p1Play.innerHTML = p1FirstCard;
-
-p2FirstCard = p2Deck[0];
-const p2Play = document.getElementById('player-2-play');
-p2Play.innerHTML = p2FirstCard;
-
-//------------Stacking cards deck------------
-// let fakeStackDeck = [];
-
-// deck.forEach(card => {
-//     fakeStackDeck.push(
-//         `<div class="fake-card">
-//             <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
-//         </div>`
-//     )
-// });
-
-// console.log(fakeStackDeck);
-// const fakeDeckContainer = document.getElementById('stack-deck');
-// fakeDeckContainer.innerHTML = fakeStackDeck.join('');
-
-
-p1FakeDeckStack = [];
-
-p1Deck.forEach(card => {
-    p1FakeDeckStack.push(
-        `<div class="p1-fake-card">
+//Draws fake stacking deck
+function drawFakeStacks(deck1, deck2) {
+    p1FakeDeckStack = [];
+    deck1.forEach(card => {
+        p1FakeDeckStack.push(
+            `<div class="p1-fake-card">
             <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
         </div>`
-    )
-})
+        )
+    })
 
-const numOfCardsLeftP1 = p1FakeDeckStack.length;
-const numOfCardsLeftElemP1 = document.getElementById("numOfCardsLeftP1");
-numOfCardsLeftElemP1.innerHTML = `Number of cards left: ${numOfCardsLeftP1}`;
+    const p1FakeDeckStackContainer = document.getElementById('p1-stack-deck');
+    p1FakeDeckStackContainer.innerHTML = p1FakeDeckStack.join('');
+    const p1fakeCards = document.querySelectorAll('.p1-fake-card');
+    p1fakeCards.forEach((card, index) => {
+        const x = index + 1;
+        const y = index + 1;
+        card.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
 
-const p1FakeDeckStackContainer = document.getElementById('p1-stack-deck');
-p1FakeDeckStackContainer.innerHTML = p1FakeDeckStack.join('');
+    const numOfCardsLeftP1 = p1FakeDeckStack.length;
+    const numOfCardsLeftElemP1 = document.getElementById("numOfCardsLeftP1");
+    numOfCardsLeftElemP1.innerHTML = `Number of cards left: ${numOfCardsLeftP1}`;
 
-const p1fakeCards = document.querySelectorAll('.p1-fake-card');
-
-p1fakeCards.forEach((card, index) => {
-  const x = index + 1;
-  const y = index + 1;
-  card.style.transform = `translateX(${x}px) translateY(${y}px)`;
-});
-
-
-p2FakeDeckStack = [];
-
-p2Deck.forEach(card => {
-    p2FakeDeckStack.push(
-        `<div class="p2-fake-card">
+    p2FakeDeckStack = [];
+    deck2.forEach(card => {
+        p2FakeDeckStack.push(
+            `<div class="p2-fake-card">
             <img src="https://media.licdn.com/dms/image/C560BAQF_9dT4QyqvWw/company-logo_200_200/0/1673266287812?e=2147483647&v=beta&t=BevULykGeF1oKA9bvQyuUm-HMcHiwTkcC-JrqwcoVsY" alt="Mindera logo">
         </div>`
-    )
-})
+        )
+    })
 
-const numOfCardsLeftP2 = p2FakeDeckStack.length;
-const numOfCardsLeftElemP2 = document.getElementById("numOfCardsLeftP2");
-numOfCardsLeftElemP2.innerHTML = `Number of cards left: ${numOfCardsLeftP2}`;
+    const p2FakeDeckStackContainer = document.getElementById('p2-stack-deck');
 
-const p2FakeDeckStackContainer = document.getElementById('p2-stack-deck');
+    p2FakeDeckStackContainer.innerHTML = p2FakeDeckStack.join('');
 
-p2FakeDeckStackContainer.innerHTML = p2FakeDeckStack.join('');
+    const p2fakeCards = document.querySelectorAll('.p2-fake-card');
 
-const p2fakeCards = document.querySelectorAll('.p2-fake-card');
+    p2fakeCards.forEach((card, index) => {
+        const x = index + 1;
+        const y = index + 1;
+        card.style.transform = `translateX(${-x}px) translateY(${y}px)`;
+    });
 
-p2fakeCards.forEach((card, index) => {
-  const x = index + 1;
-  const y = index + 1;
-  card.style.transform = `translateX(${-x}px) translateY(${y}px)`;
-});
-
-
-let counter = 0;
-function play() {
-    console.log("Hello world!")
-    counter++;
-    console.log(counter);
-
-    let counter;
-    if (counter===0) {
-        //CreateDeck();
-        //Shuffle();
-    counter ++;
-    } else {
-
-    }
-    
-    //DealEachCard();
-    //CheckValues();
-        //CheckWinner();
-        //CheckWar();
-            //Deal3BackCards() & DealEachCard();
-        //UpdateCardsLeft();       
+    const numOfCardsLeftP2 = p2FakeDeckStack.length;
+    const numOfCardsLeftElemP2 = document.getElementById("numOfCardsLeftP2");
+    numOfCardsLeftElemP2.innerHTML = `Number of cards left: ${numOfCardsLeftP2}`;
 }
+
 
 
 
